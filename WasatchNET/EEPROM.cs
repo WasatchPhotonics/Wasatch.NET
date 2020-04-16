d97dfa672ec379e868619e4ba972e8d655e32039
Mark Zieg <mark@zieg.com>
2020-04-16 11:45:30 -0400

    [2.1.20] forced wait on some async multichannel tasks

--- WasatchNET/EEPROM.cs
+++ WasatchNET/EEPROM.cs
@@ -1951,7 +1951,7 @@ namespace WasatchNET
                 wavecalCoeffs[4] = 0;
             }
 
-            if (minIntegrationTimeMS < 1 || minIntegrationTimeMS == 65535)
+            if (minIntegrationTimeMS < 1 || minIntegrationTimeMS > 1000)
             {
                 logger.error("invalid minIntegrationTimeMS found ({0}), defaulting to 1", minIntegrationTimeMS);
                 minIntegrationTimeMS = 1;
