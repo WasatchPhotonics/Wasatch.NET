bc9443988a4dfce1dd819c26f0b690d421c453fd
Mark Zieg <mark@zieg.com>
2020-05-13 12:50:17 -0400

    [multichannel-2] added FeatureMask, support serial Monte Carlo

---
+++ WasatchNET/FeatureMask.cs
@@ -0,0 +1,30 @@
+﻿using MPSSELight;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WasatchNET
+{
+    public class FeatureMask
+    {
+        public bool invertXAxis { get; set; }
+        public bool bin2x2 { get; set; }
+
+        public FeatureMask(ushort value = 0)
+        {
+            invertXAxis = 0 != (value & 0x0001);
+            bin2x2      = 0 != (value & 0x0002);
+        }
+
+        public ushort toUInt16()
+        {
+            ushort value = 0;
+            if (invertXAxis) value |= 0x0001;
+            if (bin2x2)      value |= 0x0002;
+            return value;
+        }
+    }
+}
