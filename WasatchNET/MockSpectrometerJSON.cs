bce77ea28ae80b1d2141a13ce691b1e85a5c6e5d
unknown <tstohrer@wasatchphotonics.com>
2020-07-07 15:41:53 -0400

    [2.3.2] added option for bulk loading for Mock spectrometer, for WPSC release

---
+++ WasatchNET/MockSpectrometerJSON.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WasatchNET
+{
+    class MockSpectrometerJSON
+    {
+        public Dictionary<string, SortedDictionary<int, double[]>> measurements;
+        public EEPROMJSON EEPROM;
+    }
+
+    public class EEPROMJSON
+    {
+        public string Serial;
+        public string Model;
+        public int SlitWidth;
+        public double BaudRate;
+        public bool IncBattery;
+        public bool IncCooling;
+        public bool IncLaser;
+        public int StartupIntTimeMS;
+        public int StartupTempC;
+        public int StartupTriggerMode;
+        public double DetectorGain;
+        public double DetectorGainOdd;
+        public double DetectorOffset;
+        public double DetectorOffsetOdd;
+        public double[] WavecalCoeffs;
+        public double[] TempToDACCoeffs;
+        public double[] ADCToTempCoeffs;
+        public double[] LinearityCoeffs;
+        public int DetectorTempMax;
+        public int DetectorTempMin;
+        public int ThermistorBeta;
+        public int ThermistorResAt298K;
+        public string CalibrationDate;
+        public string CalibrationBy;
+        public string DetectorName;
+        public int ActualPixelsHoriz;
+        public int ActivePixelsHoriz;
+        public int ActivePixelsVert;
+        public int ROIHorizStart;
+        public int ROIHorizEnd;
+        public double[] ROIVertRegionStarts;
+        public double[] ROIVertRegionEnds;
+        public double MaxLaserPowerMW;
+        public double MinLaserPowerMW;
+        public double ExcitationWavelengthNM;
+        public double[] BadPixels;
+        public string UserText;
+        public string ProductConfig;
+        public int RelIntCorrOrder;
+        public double[] RelIntCorrCoeff;
+    }
+
+}
