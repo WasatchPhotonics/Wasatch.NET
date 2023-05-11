3cdc954a5eb3149cd9c42d554846464ec9b25d8d
Trent Stohrer <tstohrer@wasatchphotonics.com>
2023-05-11 16:44:04 -0400

    added logger for not using winforms

--- WasatchNET/Logger.cs
+++ WasatchNET/Logger.cs
@@ -5,7 +5,9 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Globalization;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Runtime.InteropServices;
+using System.Collections.ObjectModel;
 
 namespace WasatchNET
 {
@@ -291,6 +293,321 @@ namespace WasatchNET
 
                 if (textBox != null)
                     textBox.BeginInvoke(new MethodInvoker(delegate { if (textBox != null) textBox.AppendText(msg + Environment.NewLine); }));
+            }
+        }
+    }
+
+    public class LogEntry : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public DateTime DateTime
+        {
+            get { return _DateTime; }
+            set 
+            {
+                _DateTime = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(DateTime)));
+            }
+        }
+        DateTime _DateTime;
+
+        public int Index
+        {
+            get { return _Index; }
+            set
+            {
+                _Index = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Index)));
+            }
+        }
+        int _Index;
+
+        public string Message
+        {
+            get { return _Message; }
+            set
+            {
+                _Message = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Message)));
+            }
+        }
+        string _Message;
+    }
+
+    public class WPFLogger : ILogger, INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        ////////////////////////////////////////////////////////////////////////
+        // Private attributes
+        ////////////////////////////////////////////////////////////////////////
+
+        static readonly WPFLogger instance = new WPFLogger();
+        public ObservableCollection<LogEntry> LogEntries
+        {
+            get
+            {
+                return _LogEntries;
+            }
+            set
+            {
+                _LogEntries = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(LogEntries)));
+            }
+        }
+        ObservableCollection<LogEntry> _LogEntries = new ObservableCollection<LogEntry>();
+
+        const int MAX_ERRORS = 100; // how many to queue for retrieval by getErrors()
+        LinkedList<string> errors = new LinkedList<string>();
+        int errorCount;
+
+        StreamWriter outfile;
+
+        ////////////////////////////////////////////////////////////////////////
+        // Public attributes
+        ////////////////////////////////////////////////////////////////////////
+
+        public LogLevel level
+        {
+            get => _level;
+            set
+            {
+                if (null != Environment.GetEnvironmentVariable("WASATCHNET_WPFLogger_FORCE"))
+                    return;
+
+                _level = value;
+            }
+        }
+        LogLevel _level = LogLevel.INFO;
+
+        static public WPFLogger getInstance()
+        {
+            return instance;
+        }
+
+        public void setPathname(string path)
+        {
+            if (null != Environment.GetEnvironmentVariable("WASATCHNET_WPFLogger_FORCE"))
+            {
+                debug($"WPFLogger.setPathname({path}): ignoring due to WASATCHNET_WPFLogger_FORCE");
+                return;
+            }
+
+            try
+            {
+                outfile = new StreamWriter(path);
+                info($"log path set to {path}");
+            }
+            catch (Exception e)
+            {
+                outfile = null;
+                error("Can't set log pathname: {0}", e);
+            }
+        }
+        public void setTextBox(TextBox tb) { }
+
+        public void close()
+        {
+            lock (instance)
+            {
+                if (outfile != null)
+                {
+                    outfile.Flush();
+                    outfile.Close();
+                    outfile = null;
+                }
+            }
+        }
+
+        public bool debugEnabled()
+        {
+            return level <= LogLevel.DEBUG;
+        }
+
+        public string getLastError()
+        {
+            lock (instance)
+            {
+                if (errorCount > 0)
+                {
+                    errorCount--;
+                    string msg = errors.Last.ToString();
+                    errors.RemoveLast();
+                    return msg;
+                }
+                else
+                    return null;
+            }
+        }
+
+        public List<string> getErrors()
+        {
+            lock (instance)
+            {
+                if (errorCount > 0)
+                {
+                    List<string> retval = new List<string>(errors);
+                    errors.Clear();
+                    errorCount = 0;
+                    return retval;
+                }
+                return null;
+            }
+        }
+
+        public bool hasError()
+        {
+            lock (instance)
+                return errorCount > 0;
+        }
+
+        public bool error(string fmt, params Object[] obj)
+        {
+            lock (instance)
+            {
+                // you'd think there'd be a standard collection that does this (size-limited queue/buffer)
+                errors.AddLast(String.Format(fmt, obj));
+                errorCount++;
+                while (errorCount > MAX_ERRORS)
+                {
+                    errors.RemoveFirst();
+                    errorCount--;
+                }
+            }
+
+            if (level <= LogLevel.ERROR)
+                log(LogLevel.ERROR, fmt, obj);
+
+            return false; // convenient for many cases
+        }
+
+        public void info(string fmt, params Object[] obj)
+        {
+            if (level <= LogLevel.INFO)
+                log(LogLevel.INFO, fmt, obj);
+        }
+
+        public void debug(string fmt, params Object[] obj)
+        {
+            if (level <= LogLevel.DEBUG)
+                log(LogLevel.DEBUG, fmt, obj);
+        }
+
+        public void header(string fmt, params Object[] obj)
+        {
+            if (level <= LogLevel.DEBUG)
+            {
+                debug("");
+                debug("=========================================================");
+                debug(fmt, obj);
+                debug("=========================================================");
+                debug("");
+            }
+        }
+
+        public void logString(LogLevel lvl, string msg)
+        {
+            if (level <= lvl)
+                log(lvl, msg);
+        }
+
+        public void save(string pathname)
+        {
+            try
+            {
+                TextWriter tw = new StreamWriter(pathname);
+                foreach(LogEntry logEntry in LogEntries)
+                {
+                    string msg = string.Format("{0}: {1}\n",
+                        logEntry.DateTime.ToString("yyyy-MM-dd HH:mm:ss.ffffff", CultureInfo.InvariantCulture),
+                        logEntry.Message);
+                }
+
+                tw.Close();
+            }
+            catch (Exception e)
+            {
+                error("can't write {0}: {1}", pathname, e.Message);
+            }
+        }
+
+        public void hexdump(byte[] buf, string prefix = "")
+        {
+            string line = "";
+            for (int i = 0; i < buf.Length; i++)
+            {
+                if (i % 16 == 0)
+                {
+                    if (i > 0)
+                    {
+                        debug("{0}{1}", prefix, line);
+                        line = "";
+                    }
+                    line += String.Format("{0:x4}:", i);
+                }
+                line += String.Format(" {0:x2}", buf[i]);
+            }
+            if (line.Length > 0)
+                debug("{0}{1}", prefix, line);
+        }
+
+        ////////////////////////////////////////////////////////////////////////
+        // Private methods
+        ////////////////////////////////////////////////////////////////////////
+
+        private WPFLogger()
+        {
+            var envLevel = Environment.GetEnvironmentVariable("WASATCHNET_WPFLogger_LEVEL");
+            if (envLevel != null)
+            {
+                envLevel = envLevel.ToUpper();
+                if (envLevel.Contains("DEBUG")) _level = LogLevel.DEBUG;
+                else if (envLevel.Contains("INFO")) _level = LogLevel.INFO;
+                else if (envLevel.Contains("ERR")) _level = LogLevel.ERROR;
+                else if (envLevel.Contains("NEVER")) _level = LogLevel.NEVER;
+            }
+
+            var envPathname = Environment.GetEnvironmentVariable("WASATCHNET_WPFLogger_PATHNAME");
+            if (envPathname != null)
+            {
+                // object isn't sufficiently constructed enough to use setPathname()?
+                try
+                {
+                    outfile = new StreamWriter(envPathname);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Can't set log pathname: {e}");
+                }
+            }
+        }
+
+        string getTimestamp()
+        {
+            return DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.ffffff", CultureInfo.InvariantCulture);
+        }
+
+        void log(LogLevel lvl, string fmt, params Object[] obj)
+        {
+            string threadName = null;
+            if (Thread.CurrentThread.Name != null)
+                threadName = $"[{Thread.CurrentThread.Name}] ";
+            else if (Task.CurrentId != null)
+                threadName = $"[Task 0x{Task.CurrentId:x4}] ";
+
+            string msg = string.Format("{0}{1}: {2}",
+                threadName,
+                lvl,
+                string.Format(fmt, obj));
+
+            LogEntries.Add(new LogEntry() { DateTime = DateTime.Now, Index = LogEntries.Count, Message = msg });
+
+            lock (instance)
+            {
+                if (outfile != null && outfile.BaseStream != null)
+                {
+                    outfile.WriteLine(msg);
+                    outfile.Flush();
+                }
             }
         }
     }
