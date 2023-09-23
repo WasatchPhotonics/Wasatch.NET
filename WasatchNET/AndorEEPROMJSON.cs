01e9cea457560a02015871ffdd76e97cd5ebb43b
Mark Zieg <mzieg@wasatchphotonics.com>
2023-09-23 09:22:33 -0400

    added

---
+++ WasatchNET/AndorEEPROMJSON.cs
@@ -0,0 +1,12 @@
+﻿namespace WasatchNET
+{
+    public class AndorEEPROMJSON
+    {
+        public string detector_serial_number;
+        public string detector_type;
+        public double[] wavelength_coeffs;
+        public double excitation_nm_float;
+        public string wp_serial_number;
+        public string wp_model;
+    }
+}
